e031dc18d22f8894fb8394674346f6d1759be31c
Donald McKendrick <ddmckendrick@gmail.com>
2013-04-11 01:38:24 +0200

    Improves completion rate. New Levenshtein function
    Much simpler Levenshtein Distance code that gives higher completion rate at a much faster speed.

--- weasel.rb
+++ weasel.rb
@@ -1,27 +1,3 @@
-def levenshtein_distance(s, t)
-  m = s.length
-  n = t.length
-  return m if n == 0
-  return n if m == 0
-  d = Array.new(m+1) {Array.new(n+1)}
- 
-  (0..m).each {|i| d[i][0] = i}
-  (0..n).each {|j| d[0][j] = j}
-  (1..n).each do |j|
-    (1..m).each do |i|
-      d[i][j] = if s[i-1] == t[j-1]  # adjust index into string
-                  d[i-1][j-1]       # no operation required
-                else
-                  [ d[i-1][j]+1,    # deletion
-                    d[i][j-1]+1,    # insertion
-                    d[i-1][j-1]+1,  # substitution
-                  ].min
-                end
-    end
-  end
-  d[m][n]
-end
-
 def stringGen(sample)
   counter = 0
   result = "" 
@@ -65,8 +41,8 @@ def pickFittest(prev_fit, generation, sample, target)
   fittest = prev_fit
   counter = 0
   until counter == generation.length
-    if levenshtein_distance(generation[counter], sample) <= target
-      target = levenshtein_distance(generation[counter], sample)
+    if donLeven(generation[counter], sample) <= target
+      target = donLeven(generation[counter], sample)
       fittest = generation[counter]
     end
     counter += 1
@@ -74,18 +50,34 @@ def pickFittest(prev_fit, generation, sample, target)
   return fittest, target
 end
 
+def donLeven(attempt, target)
+  counter = 0
+  score = 0
+  until counter == target.length
+    unless attempt[counter] == target[counter]
+      score +=1
+    end
+    counter += 1
+  end
+  return score
+end
+
 SAMPLE = "methinks it is a weasel"
 
 generation = multiplier(stringGen(SAMPLE), 100)
 
 fittest, target = pickFittest(generation[0], generation, SAMPLE, SAMPLE.length)
-puts "fittest = #{fittest} - #{levenshtein_distance(fittest, SAMPLE)}"
+puts "fittest = #{fittest} - #{donLeven(fittest, SAMPLE)}"
 
 puts SAMPLE.length
 counter = 1
-until levenshtein_distance(fittest, SAMPLE) == 0 or counter == 100
+
+puts donLeven(generation[0], SAMPLE)
+puts levenshtein_distance(generation[0], SAMPLE)
+
+until donLeven(fittest, SAMPLE) == 0 or counter == 100
   generation = multiplier(fittest, 100)
   fittest, target = pickFittest(fittest, generation, SAMPLE, target)
-  puts "fittest = #{fittest} - #{levenshtein_distance(fittest, SAMPLE)} count: #{counter}"
+  puts "fittest = #{fittest} - #{donLeven(fittest, SAMPLE)} count: #{counter}"
   counter += 1
 end
